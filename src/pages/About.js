7804c550a4e4df121edcc7f04a2aae1d2319f276
aman <bhandari.aman0101@gmail.com>
2023-01-10 12:48:47 +0530

    navbar,routes,about,error

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className='section about-section'>
+      <h1 className='section-title'>About us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil officiis,
+        repellat voluptate eos ratione reiciendis ipsa libero corrupti, tempore,
+        labore magni tempora corporis voluptatum! In esse vitae itaque quaerat
+        aliquid.
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
